fc6ac972f1b695532839bca3b771fa4956707d04
Coursera Learner <nobody@nowhere.nul>
2020-05-08 05:44:26 +0000

    Did assignment 6

--- 06_rect/rectangle.c
+++ 06_rect/rectangle.c
@@ -16,14 +16,64 @@ int max (int a, int b) {
 }
 
 //Declare your rectangle structure here!
+struct rct_tag{
+  int x;
+  int y;
+  int width;
+  int height;
+};
+typedef struct rct_tag rectangle;
 
 
 rectangle canonicalize(rectangle r) {
   //WRITE THIS FUNCTION
+  if (r.width<0){
+    r.x =r.x +r.width;
+    r.width = r.width *-1;
+  }
+  else if (r.height<0){
+    r.y=r.y+r.height;
+    r.height=r.height*-1;
+  }
   return r;
 }
 rectangle intersection(rectangle r1, rectangle r2) {
   //WRITE THIS FUNCTION
+  r1 = canonicalize(r1);
+  r2 = canonicalize(r2);
+  if (((r2.x<=r1.x+r1.width) && (r1.x<=r2.x))&&((r2.y<=r1.y+r1.height) && (r1.y<=r2.y))){
+    r1.width=min((r2.x+r2.width),(r1.x+r1.width))-r2.x;
+    r1.height=min((r2.y+r2.height),(r1.y+r1.height))-r2.y;
+    r1.x=r2.x;
+    r1.y=r2.y;
+  }
+  else if (((r1.x<=r2.x+r2.width) && (r2.x<=r1.x))&&((r1.y<=r2.y+r2.height) && (r2.y<=r1.y))){
+    r1.width=min((r1.x+r1.width),(r2.x+r2.width))-r1.x;
+    r1.height=min((r1.y+r1.height),(r2.y+r2.height))-r1.y;
+  }
+  else if ((r1.y<=r2.y+r2.height)&&(r1.y>=r2.y)&&(r2.x>=r1.x)&&(r2.x<r1.x+r1.width)){
+    r1.height=min((r2.y+r2.height-r1.y),(r2.height));
+    r1.width=min((r1.width+r1.x-r2.x),(r2.width));
+    r1.x=r2.x;
+  }
+  else if ((r2.y<=r1.y+r1.height)&&(r2.y>=r1.y)&&(r1.x>=r2.x)&&(r1.x<r2.x+r2.width)){
+    r1.height=min((r1.y+r1.height-r2.y),(r1.height));
+    r1.width=min((r2.width+r2.x-r1.x),(r1.width));
+    r1.y=r2.y;
+  }
+  else if ((r1.x<=r2.x+r2.width)&&(r1.x>=r2.x)&&(r1.y<r2.y)&&(r1.y+r1.height>r2.y+r2.height)){
+      r1.height=r2.height;
+      r1.width=r2.x+r2.width-r1.x;
+      r1.y=r2.y;
+  }
+  else if ((r2.x<=r1.x+r1.width)&&(r2.x>=r1.x)&&(r2.y<r1.y)&&(r2.y+r2.height>r1.y+r1.height)){
+      r1.width=r1.x+r1.width-r2.x;
+      r1.x=r2.x;
+  }
+  else {
+      r1.width=0;
+      r1.height=0;
+  }
   return r1;
 }
 
@@ -53,9 +103,9 @@ int main (void) {
   printRectangle(r1);
 
   r2.x = 4;
-  r2.y = 5;
+  r2.y = -2;
   r2.width = -5;
-  r2.height = -7;
+  r2.height = 7;
   printf("r2 is ");
   printRectangle(r2);
   
