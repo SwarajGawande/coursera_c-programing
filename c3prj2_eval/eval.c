69fe1c05989b40358c8b32f64817235ceabfc138
Coursera Learner <nobody@nowhere.nul>
2020-05-25 05:19:04 +0000

    Did c3prj2_eval

--- c3prj2_eval/eval.c
+++ c3prj2_eval/eval.c
@@ -169,7 +169,8 @@ int compare_hands(deck_t * hand1, deck_t * hand2) {
   }
   if (hand1_eval.ranking==hand2_eval.ranking){
     int i=0;
-    while((*(hand1_eval.cards[i])).value==(*(hand2_eval.cards[i])).value&&(*(hand1_eval.cards[i])).suit==(*(hand2_eval.cards[i])).suit&&i<5){
+    while((*(hand1_eval.cards[i])).value==(*(hand2_eval.cards[i])).value
+	  &&(*(hand1_eval.cards[i])).suit==(*(hand2_eval.cards[i])).suit&&i<4){
       i=i+1;
     }
     if ((*(hand1_eval.cards[i])).value!=(*(hand2_eval.cards[i])).value){
