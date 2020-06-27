419c775aee7b29c34cc7a70dce28f7e5c693ba4b
Coursera Learner <nobody@nowhere.nul>
2020-06-27 07:29:21 +0000

    Day 3

--- c4prj2_input/input.c
+++ c4prj2_input/input.c
@@ -18,7 +18,7 @@ deck_t * hand_from_string(const char*str,future_cards_t *fc){
     }
     if(str[i]=='?'){
       int j=0;
-      char* num[len];
+      char num[len];
       while(str[i]!='\0'&&str[i]!=' '){
 	num[j]=str[i];
 	i++;
