94effa9f823b8a76478ca288680613d4fc564b41
Coursera Learner <nobody@nowhere.nul>
2020-06-27 05:33:32 +0000

    Day 3

--- c4prj2_input/input.c
+++ c4prj2_input/input.c
@@ -22,7 +22,7 @@ deck_t * hand_from_string(const char*str,future_cards_t *fc){
       card_t * ptr=add_empty_card(hand);
       add_future_card(fc,n,ptr);
       //hand->n_cards++;
-      i=i+2;//+n/10;
+      i=i+2+n/10;
       continue;
     }
     hand->cards=realloc(hand->cards,(hand->n_cards+1)*sizeof(*hand->cards));
